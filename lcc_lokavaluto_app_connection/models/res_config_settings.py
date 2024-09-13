7829268c94b7ff19391c2a5e5f637c6649cebf9e
Laetitia Da Costa <laetitia.dacosta@elabore.coop>
2024-09-13 12:01:17 +0200

    [IMP]lcc_lokavaluto_app_connection:allow or not sending allocation to individuals

--- lcc_lokavaluto_app_connection/models/res_config_settings.py
+++ lcc_lokavaluto_app_connection/models/res_config_settings.py
@@ -34,3 +34,9 @@ class ResConfigSettings(models.TransientModel):
         readonly=False,
         string="Commission Product",
     )
+
+    allow_payments_only_to_companies = fields.Boolean(
+        related="company_id.allow_payments_only_to_companies",
+        readonly=False,
+        string="Allow payments only to companies",
+    )
