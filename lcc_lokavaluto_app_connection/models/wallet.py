b6c30a87dfcee49b1e3372dc28eb03dbfbf076d0
Stéphan Sainléger <contact@stephansainleger.fr>
2024-11-07 19:42:22 +0100

    [IMP] lcc_lokavaluto_app_connection: prevent wallet delete with security rules

--- lcc_lokavaluto_app_connection/models/wallet.py
+++ lcc_lokavaluto_app_connection/models/wallet.py
@@ -52,9 +52,6 @@ class ResPartnerBackend(models.Model):
             if record.partner_id.public_profile_id:
                 record.partner_public_name = record.partner_id.public_profile_id.name
 
-    def unlink(self):
-        raise UserError("You can't delete a numeric wallet. Please archive it instead.")
-
     def get_lcc_product(self):
         """Return the numeric lcc product to add in sale orders or invoices.
         Need to be overrided by financial backend add-ons"""
